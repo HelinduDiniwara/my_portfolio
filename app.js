76ed495f2ecbd9557188fd385228dd5cd552bc91
HelinduDiniwara <helindudiniwara@gmail.com>
2024-11-26 03:22:03 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text",{
+    strings: ["Full-Stack Developer","Frontend Developer","Backend Developer"],
+    typeSpeed: 40,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
